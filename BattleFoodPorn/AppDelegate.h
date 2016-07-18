e1a79f466c86b851a05d6ebb255b73c7766e529f
Candance Smith <Candance@candances-macbook-air.local>
2016-07-18 17:06:19 -0400

    Initial Commit

---
+++ BattleFoodPorn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BattleFoodPorn
+//
+//  Created by Candance Smith on 7/18/16.
+//  Copyright © 2016 candance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BattleFoodPorn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BattleFoodPorn
+//
+//  Created by Candance Smith on 7/18/16.
+//  Copyright © 2016 candance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
